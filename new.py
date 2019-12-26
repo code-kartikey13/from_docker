52ac61bc5e0c2043944ac9de01c61d6e31bef06c
Code-Kartikey12 <38080424+Code-Kartikey12@users.noreply.github.com>
2019-12-26 14:49:33 +0530

    added sub def()

--- new.py
+++ new.py
@@ -2,7 +2,12 @@ a=5
 b=5
 c=5
 def sum(a,b,c):
-  d = a+b+c
-  return d
+    d = a+b+c
+    return d
 
-print(sum(a,b,c))
+def sub(a,b,c,d):
+  e = d-(a+b)
+  return e
+
+print(f"sum of three numbers: {sum(a,b,c)}")
+print(f"sub -> {sub(a,b,c,sum(a,b,c))}")
